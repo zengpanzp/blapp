9fd1fcd1e11c61cb4ccd3c1545c3adf86a82baa4
lilu <v-Lu.Li@bl.com>
2017-03-16 19:10:51 +0800

    增加入口准备调试H5请求服务

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 /* creat by chenpeng */
 
 /*
-* 路由管理
-*/
+ * 路由管理
+ */
 
 import Vue from 'vue'
 import Router from 'vue-router'
@@ -10,44 +10,50 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      redirect: '/electricMainPage'
-    },
-    /* 陈鹏 资源位跳转 */
-    // {
-    //   path: '/resourceRouter',
-    //   component: r => require.ensure([], () => r(require('../views/resourceRouter')), 'resourceRouter')
-    // },
-    /* 陈鹏 精品闪购首页 */
-    {
-      path: '/flashSales',
-      meta: {
-        title: '精品闪购'
-      },
-      component: r => require.ensure([], () => r(require('../views/flashSale/flashSales')), 'flashSales')
-    },
-    /* 陈鹏 精品闪购活动列表页 */
-    {
-      path: '/flashsaleproductspage/:flashId/:isStart',
-      component: r => require.ensure([], () => r(require('../views/flashSale/productsListView')), 'flashsaleproductspage')
-    },
-    /* 陈鹏 电器城首页 */
-    {
-      path: '/electricMainPage',
-      meta: {
-        title: '电器城'
-      },
-      component: r => require.ensure([], () => r(require('../views/electricCity/electricHome')), 'electricMainPage')
-    },
-    /* 陈鹏 值得买首页 */
-    {
-      path: '/rankMainPage',
-      meta: {
-        title: '大家都在买'
-      },
-      component: r => require.ensure([], () => r(require('../views/rank/mainView')), 'rankMainPage')
-    }
-  ]
+    routes: [{
+            path: '/',
+            redirect: '/electricMainPage'
+        },
+        /* 陈鹏 资源位跳转 */
+        // {
+        //   path: '/resourceRouter',
+        //   component: r => require.ensure([], () => r(require('../views/resourceRouter')), 'resourceRouter')
+        // },
+        /* 陈鹏 精品闪购首页 */
+        {
+            path: '/flashSales',
+            meta: {
+                title: '精品闪购'
+            },
+            component: r => require.ensure([], () => r(require('../views/flashSale/flashSales')), 'flashSales')
+        },
+        /* 陈鹏 精品闪购活动列表页 */
+        {
+            path: '/flashsaleproductspage/:flashId/:isStart',
+            component: r => require.ensure([], () => r(require('../views/flashSale/productsListView')), 'flashsaleproductspage')
+        },
+        /* 陈鹏 电器城首页 */
+        {
+            path: '/electricMainPage',
+            meta: {
+                title: '电器城'
+            },
+            component: r => require.ensure([], () => r(require('../views/electricCity/electricHome')), 'electricMainPage')
+        },
+        /* 陈鹏 值得买首页 */
+        {
+            path: '/rankMainPage',
+            meta: {
+                title: '大家都在买'
+            },
+            component: r => require.ensure([], () => r(require('../views/rank/mainView')), 'rankMainPage')
+        },
+        /**增加入口 */
+        {
+            path: '/ProductList',
+            meta: {
+                title: '商品列表'
+            }
+        }
+    ]
 })
