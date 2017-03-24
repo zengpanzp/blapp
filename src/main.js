56ac59cf2919352e95812e58b3299061b69c22ba
陈鹏 <v-peng.chen@bl.com>
2017-03-24 12:33:46 +0800

    scroll-top指令优化

--- src/main.js
+++ src/main.js
@@ -80,25 +80,33 @@ Vue.directive('scroll-fixed', function(el) {
 /* 返回顶部指令 */
 Vue.directive('scroll-top', {
 
-  bind: function (el) {
+  bind: function (el, binding) {
     let ducDiv = document.createElement('div')
     let winHeight = document.documentElement.clientHeight
-    ducDiv.className = 'gotop'
-    ducDiv.style.display = 'none'
-    if (el.parentNode) {
-      el.parentNode.appendChild(ducDiv)
-    }
-    ducDiv.addEventListener('click', function() {
-      ScrollTo.top(el, 0)
-    })
-    el.addEventListener('scroll', function () {
-      let thisScrollTop = el.scrollTop
+    let oEl = binding.modifiers.window ? document.body : el
+    let fnToggleShow = () => {
+      let thisScrollTop = oEl.scrollTop
       if (thisScrollTop > winHeight) {
         ducDiv.style.display = 'block'
       } else {
         ducDiv.style.display = 'none'
       }
+    }
+    ducDiv.className = 'gotop'
+    ducDiv.style.display = 'none'
+    if (el.parentNode && !binding.modifiers.window) {
+      el.parentNode.appendChild(ducDiv)
+    } else {
+      el.appendChild(ducDiv)
+    }
+    ducDiv.addEventListener('click', function() {
+      ScrollTo.top(oEl, 0)
     })
+    if (binding.modifiers.window) {
+      window.addEventListener('scroll', fnToggleShow, false)
+    } else {
+      oEl.addEventListener('scroll', fnToggleShow, false)
+    }
   }
 })
 
