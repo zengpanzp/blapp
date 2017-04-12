6424b8fd59f2848dc9696d1df12c57e4f95c234d
陈鹏 <v-peng.chen@bl.com>
2017-04-12 12:44:17 +0800

    guide

--- src/views/UI/guide.vue
+++ src/views/UI/guide.vue
@@ -1,23 +1,32 @@
 <template>
   <div id="index">
     <h2>iBaiappVue项目 页面索引</h2>
-    <a href="/#/ui/ecardList">电子卡详情</a>
-    <a href="/#/ui/eleCard">电子卡</a>
+    <h3 class="second-title">ui路由</h3>
+    <router-link :to="{ path: item.path }" v-if="item.meta" v-for="(item, index) in auiRouter">{{ item.meta.title }}</router-link>
+
+    <h3 class="second-title">prd路由</h3>
+    <router-link :to="{ path: item.path }" v-if="item.meta" v-for="(item, index) in aprdRouter">{{ item.meta.title }}</router-link>
   </div>
 </template>
 
 <script>
+import { uiRouter } from 'src/router/uiRouter'
+import { prdRouter } from 'src/router/prdRouter'
+
 export default {
 
   name: 'keepGuide',
 
   data () {
     return {
-
+      auiRouter: [],
+      aprdRouter: []
     };
   },
   activated() {
     this.$loading.close()
+    this.auiRouter = uiRouter
+    this.aprdRouter = prdRouter
   }
 };
 </script>
@@ -36,5 +45,9 @@ export default {
     padding: rem(15);
     font-size: rem(28)
   }
+}
+.second-title{
+  text-align: center;
+  margin: 20px 0;
 }
 </style>
