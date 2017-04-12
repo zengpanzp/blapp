befa3d852ae6d9020408be3e980283271cafe3a1
陈鹏 <v-peng.chen@bl.com>
2017-04-12 13:25:21 +0800

    优化组件

---
+++ src/pre-UrlConfig.js
@@ -0,0 +1,8 @@
+let doMain = 'http://m.st.bl.com/h5-web' // 分享h5网址
+let serviceBaseUrl = 'http://10.201.128.110:7210'; // 接口请求
+
+export default {
+  H5_FLASH: `${doMain}/flash/goflash.html`,
+  H_FLASHSHOP: `${doMain}/flash/viewFlashBuyShop.html?`,
+  SERVICE_BASE_URL: `${serviceBaseUrl}`
+}
