bdf9686ebafe887d97278eef2cbd0e8f2b7f0cd8
shenmashi <mashi.shen@bl.com>
2017-04-12 11:05:04 +0800

    new

--- src/api/index.js
+++ src/api/index.js
@@ -32,50 +32,6 @@ let baseUrl = urlConfig.SERVICE_BASE_URL;
 // Axios.defaults.headers.post['Content-Type'] = 'application/json';
 // Axios.defaults.headers.post['chnflg'] = 'h5';
 export default {
-  // token 验证
-  auth: (params) => {
-    return (
-      Vue.http.interceptors.push((request, next) => {
-        if (params.token) {
-          request.headers.set('token', params.token)
-        }
-        next((response) => {
-          return response
-        });
-      }),
-      Vue.resource(baseUrl + '/auth').get()
-    )
-  },
-  // 登录
-  login: (params) => {
-    return (
-      Vue.http.post(baseUrl + '/login/web', params)
-    )
-  },
-  // 退出登录
-  forget: (params) => {
-    return (
-      Vue.http.post(baseUrl + '/forget', params)
-    )
-  },
-  // buyProducts
-  buyProducts: (params) => {
-    return (
-      Vue.http.post(baseUrl + '/buyProducts', params)
-    )
-  },
-  // getProducts
-  getProducts: (params) => {
-    return (
-      Vue.http.post(baseUrl + '/getproducts', params)
-    )
-  },
-  // getFlashSlides
-  getFlashSlides: (params) => {
-    return (
-      Vue.http.post(baseUrl + '/getFlashSlides', params)
-    )
-  },
   // 获得券列表
   getCouponList: (params) => {
     return (
