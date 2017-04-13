c8b00195eb47afe0c05b040873b8f17d20ef36fc
陈鹏 <v-peng.chen@bl.com>
2017-04-13 19:29:43 +0800

    up

--- src/views/E-card/ecardList.vue
+++ src/views/E-card/ecardList.vue
@@ -1,20 +1,19 @@
 <template>
   <div :class="{manage:!more}" >
-      <div class="ecard-order margin-b">
-        <div class="order-box">
-          <div class="label-box"><span>订单编号：</span>BPE20150931052095</div>
-          <div class="label-box"><span>下单时间：</span>2017-03-01</div>
-        </div>
-      </div>
+    <div class="order-box">
+      <div class="label-box"><span>订单编号：</span>BPE20150931052095</div>
+      <div class="label-box"><span>下单时间：</span>2017-03-01</div>
+    </div>
+    <bl-scroll :enableRefresh="false" :on-infinite="onInfinite" :enableInfinite="isLoading" v-scroll-top v-scroll-fixed>
       <div class="card-list">
         <bl-swipeout>
-          <bl-swipeout-item class="swipe-contain margin-b" transition-mode="follow" v-for="(item, index) in cardList">
+          <bl-swipeout-item class="swipe-contain margin-b" :disabled="swipeoutDisabled" transition-mode="follow" v-for="(item, index) in cardList">
             <div slot="right-menu">
-              <button class="vux-swipeout-button show-pass" @click="transPass(item.cardPin, index)">显示<br>密码</button>
+              <button class="vux-swipeout-button show-pass" @click="transPass(item.cardPin, index)" :disabled="item.cardStatus == '06'">显示<br>密码</button>
             </div>
             <div slot="content" class="swiper-left">
               <div class="select-box">
-                <div class="circle-select " v-show='!more'></div>
+                <div class="circle-select" v-show='!more'></div>
               </div>
               <div class="card-box">
                 <div class="card-num">
@@ -40,12 +39,13 @@
           </bl-swipeout-item>
         </bl-swipeout>
       </div>
-      <div class="manage-button">
-        <div class="button-box">
-          <button class="show-pass">显示密码</button>
-        </div>
+    </bl-scroll>
+    <div class="manage-button">
+      <div class="button-box">
+        <button class="show-pass" @click="cancleSelect">显示密码</button>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -57,36 +57,49 @@ export default {
   data () {
     return {
       more: true,
+      isLoading: true,
+      swipeoutDisabled: false,
+
+      currentPage: 1,
       cardList: []
     };
   },
   created() {
-    api.payRed({
-      body: {
-        pageSize: "10",
-        currentPage: "1"
-      },
-      header: {
-        merOrderNo: "llTest20170117110500"
-      }
-    }).then(data => {
-      this.$loading.close()
-      let resData = JSON.parse(data.body.obj)
-      resData.body.cardList.forEach((item) => {
-        item.showPass = false
-        item.pheredText = '•••• •••• •••• ••••'
-      })
-      this.cardList = resData.body.cardList
-    }, err => {
-      console.log(err)
-    })
+    this.fetchCardList()
   },
   methods: {
-    showmore() {
-      this.more = !this.more
+    onInfinite(done) {
+      this.fetchCardList(done)
+    },
+    fetchCardList(done) {
+      api.payRed({
+        body: {
+          pageSize: "10",
+          currentPage: this.currentPage ++
+        },
+        header: {
+          merOrderNo: "llTest20170117110500"
+        }
+      }).then(data => {
+        this.$loading.close()
+        let resData = JSON.parse(data.body.obj)
+        resData.body.cardList.forEach((item) => {
+          item.showPass = false
+          item.pheredText = '•••• •••• •••• ••••'
+        })
+        this.cardList = this.cardList.concat(resData.body.cardList)
+        if (this.cardList.length < 10) {
+          this.isLoading = false
+        }
+        if (done) {
+          done()
+        }
+      }, err => {
+        console.log(err)
+      })
     },
     fnStatus(val) {
-      let aStatus = ['', '在仓', '已领用', '已提卡', '已激活', '已作废', '已冻结', '已过期']
+      let aStatus = ['', '在仓', '可售', '已提卡', '已激活', '已作废', '已冻结', '已过期']
       return aStatus[parseInt(val)]
     },
     transPass(val, index) {
@@ -110,7 +123,22 @@ export default {
           }
         })
       }
-    }
+    },
+    // 下面方法给native调用
+    fullSelect() {},
+    cancleSelect() {
+      this.more = true
+      this.swipeoutDisabled = false
+    },
+    manageSelect() {
+      this.swipeoutDisabled = true
+      this.more = false
+    },
+  },
+  mounted() {
+    window.fullSelect = this.fullSelect
+    window.cancleSelect = this.cancleSelect
+    window.manageSelect = this.manageSelect
   }
 };
 </script>
