a1fcbea612185f6e3b3501ee47b49fcdfc8687c3
陈鹏 <v-peng.chen@bl.com>
2017-05-22 13:57:26 +0800

    up

--- src/views/electricCity/electricHome.vue
+++ src/views/electricCity/electricHome.vue
@@ -1,113 +1,115 @@
 <style lang="scss" src="./css/_electricHome.scss" scoped></style>
 <template>
-  <div class="new" v-scroll-top.window>
-    <form id="searchForm" v-on:submit.prevent="search">
-      <div class="nav-app">
-        <div class="nav-search col">
-          <input type="search" class="nav-search-input" name="" placeholder="空调" v-model="searchText">
+  <div class="main" v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="10">
+    <div class="new" v-scroll-top.window>
+      <form id="searchForm" v-on:submit.prevent="search">
+        <div class="nav-app">
+          <div class="nav-search col">
+            <input type="search" class="nav-search-input" name="" placeholder="空调" v-model="searchText">
+          </div>
         </div>
-      </div>
-    </form>
-    <!-- 轮播图 -->
-    <bl-slide class="electric-swipe" :slides="filterAllSlides" :autoPlay="true"></bl-slide>
-    <!-- end -->
-    <div class="row-list">
-      <a href="javascript:;" class="list-item col" v-for="{ imgUrl, text, id } in listHead" @click="goNative(id)">
-        <div class="item-hd"><img :src="imgUrl" alt=""></div>
-        <div class="item-bd">{{ text }}</div>
-      </a>
-    </div>
-    <div class="row-box hot-pro">
-      <div class="pro-hd">
-        <div class="popularity-img"></div>
-      </div>
-      <div class="pro-hd-list js-proscroll">
-        <ul id="content">
-          <li v-for="item in filterListRows">
-            <a href="javascript:;" v-go-native-goods-detail="item">
-              <div class="col-img col lazy-box"><img class="lazy" v-lazy.content="{ src: item.goodsImgPath }" alt=""></div>
-              <div class="pro-text">{{ item.productName }}</div>
-              <div class="pro-price red"><span class="small-price">￥</span>{{ item.goodsPrice }}</div>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="three-minor">
-      <div class="aside-left col" v-if="hotTwoBrands && hotTwoBrands.mediaUrl">
-        <a href="javascript:;" class="lazyload" v-go-native-resource="hotTwoBrands">
-          <img v-lazy="{ src: hotTwoBrands.mediaUrl }" :alt="hotTwoBrands.deployName" />
+      </form>
+      <!-- 轮播图 -->
+      <bl-slide class="electric-swipe" :slides="filterAllSlides" :autoPlay="true"></bl-slide>
+      <!-- end -->
+      <div class="row-list">
+        <a href="javascript:;" class="list-item col" v-for="{ imgUrl, text, id } in listHead" @click="goNative(id)">
+          <div class="item-hd"><img :src="imgUrl" alt=""></div>
+          <div class="item-bd">{{ text }}</div>
         </a>
       </div>
-      <div class="aside-right col">
-        <div class="aside-section hairline-top lazyload" v-if="asideTwo && asideTwo.mediaUrl">
-          <a href="javascript:;" v-go-native-resource="asideTwo">
-            <img v-lazy="{ src: asideTwo.mediaUrl }" :alt="asideTwo.deployName" />
-          </a>
+      <div class="row-box hot-pro">
+        <div class="pro-hd">
+          <div class="popularity-img"></div>
         </div>
-        <div class="aside-section lazyload" v-if="asideOne && asideOne.mediaUrl">
-          <a href="javascript:;" v-go-native-resource="asideOne">
-            <img v-lazy="{ src: asideOne.mediaUrl }" :alt="asideOne.deployName" />
-          </a>
+        <div class="pro-hd-list js-proscroll">
+          <ul id="content">
+            <li v-for="item in filterListRows">
+              <a href="javascript:;" v-go-native-goods-detail="item">
+                <div class="col-img col lazy-box"><img class="lazy" v-lazy.content="{ src: item.goodsImgPath }" alt=""></div>
+                <div class="pro-text">{{ item.productName }}</div>
+                <div class="pro-price red"><span class="small-price">￥</span>{{ item.goodsPrice }}</div>
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
-    </div>
-    <div class="sales-promotion row-box">
-      <div class="pro-hd">
-        <div class="promotion-img"></div>
-      </div>
-      <div class="pro-hd-list">
-        <ul>
-          <li class="lazyload" v-for="item in filterPromotion" v-go-native-resource="item">
-            <a href="javascript:;">
-              <div class="sub-title">{{ item.deployName }}</div>
-              <div class="col-img"><img v-lazy="{ src: item.mediaUrl }" :alt="item.deployName"></div>
+      <div class="three-minor">
+        <div class="aside-left col" v-if="hotTwoBrands && hotTwoBrands.mediaUrl">
+          <a href="javascript:;" class="lazyload" v-go-native-resource="hotTwoBrands">
+            <img v-lazy="{ src: hotTwoBrands.mediaUrl }" :alt="hotTwoBrands.deployName" />
+          </a>
+        </div>
+        <div class="aside-right col">
+          <div class="aside-section hairline-top lazyload" v-if="asideTwo && asideTwo.mediaUrl">
+            <a href="javascript:;" v-go-native-resource="asideTwo">
+              <img v-lazy="{ src: asideTwo.mediaUrl }" :alt="asideTwo.deployName" />
             </a>
-            <div class="sup-con">{{ item.picDesc1 }}</div>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="row-box hot-pro">
-      <div class="pro-hd">
-        <div class="hotbrand-img"></div>
-      </div>
-      <div class="hot-bd">
-        <div class="bd-1of3 col lazyload" v-if="hotBrands && hotBrands.mediaUrl">
-          <a :href="hotBrands.jumpUrl">
-            <img v-lazy="{ src: hotBrands.mediaUrl }" :alt="hotBrands.deployName" />
-          </a>
+          </div>
+          <div class="aside-section lazyload" v-if="asideOne && asideOne.mediaUrl">
+            <a href="javascript:;" v-go-native-resource="asideOne">
+              <img v-lazy="{ src: asideOne.mediaUrl }" :alt="asideOne.deployName" />
+            </a>
+          </div>
         </div>
-        <div class="bd-2of3 col">
-          <div class="clearfix">
-            <div class="bd-in-1of2" v-for="item in filterBrands" v-go-native-resource="item">
-              <a href="javascript:;" class="lazyload"><img class="pp-title" v-lazy="{ src: item.mediaUrl }"></a>
+      </div>
+      <div class="sales-promotion row-box">
+        <div class="pro-hd">
+          <div class="promotion-img"></div>
+        </div>
+        <div class="pro-hd-list">
+          <ul>
+            <li class="lazyload" v-for="item in filterPromotion" v-go-native-resource="item">
+              <a href="javascript:;">
+                <div class="sub-title">{{ item.deployName }}</div>
+                <div class="col-img"><img v-lazy="{ src: item.mediaUrl }" :alt="item.deployName"></div>
+              </a>
+              <div class="sup-con">{{ item.picDesc1 }}</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="row-box hot-pro">
+        <div class="pro-hd">
+          <div class="hotbrand-img"></div>
+        </div>
+        <div class="hot-bd">
+          <div class="bd-1of3 col lazyload" v-if="hotBrands && hotBrands.mediaUrl">
+            <a :href="hotBrands.jumpUrl">
+              <img v-lazy="{ src: hotBrands.mediaUrl }" :alt="hotBrands.deployName" />
+            </a>
+          </div>
+          <div class="bd-2of3 col">
+            <div class="clearfix">
+              <div class="bd-in-1of2" v-for="item in filterBrands" v-go-native-resource="item">
+                <a href="javascript:;" class="lazyload"><img class="pp-title" v-lazy="{ src: item.mediaUrl }"></a>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="appliance-tab">
-      <div class="tab-hairs">
-        <div class="tab-title js-proscroll">
-          <bl-navbar class="js-sctab" v-model="tabsModel">
-            <bl-tab-item class="el-tab-item" v-for="({ jumpId, deployName }, index) in filterEleTabs" :id="index" @click.native="elTabItem(jumpId)"><span>{{ deployName }}</span></bl-tab-item>
-          </bl-navbar>
+      <div class="appliance-tab">
+        <div class="tab-hairs">
+          <div class="tab-title js-proscroll">
+            <bl-navbar class="js-sctab" v-model="tabsModel">
+              <bl-tab-item class="el-tab-item" v-for="({ jumpId, deployName }, index) in filterEleTabs" :id="index" @click.native="elTabItem(jumpId)"><span>{{ deployName }}</span></bl-tab-item>
+            </bl-navbar>
+          </div>
         </div>
-      </div>
-      <div class="tab-content">
-        <div class="tab-c-item">
-          <a class="item-href" href="javascript:;" v-for="item in filterTabRows" :key="item.goodsId" v-go-native-goods-detail="item">
-            <div class="col c-item-img lazy-box"><img class="lazy" v-lazy="{ src: item.goodsImgPath }" alt=""></div>
-            <div class="col c-item-text">
-              <div class="item-text1">{{ item.productName }}</div>
-              <div class="item-text2">{{ item.goodsMsg }}</div>
-              <div class="red item-text3"><span class="middle-price">￥</span>{{ item.goodsPrice }}</div>
-            </div>
-          </a>
-        </div>
-        <div class="tab-end">
-          - END -
+        <div class="tab-content">
+          <div class="tab-c-item">
+            <a class="item-href" href="javascript:;" v-for="item in filterTabRows" :key="item.goodsId" v-go-native-goods-detail="item">
+              <div class="col c-item-img lazy-box"><img class="lazy" v-lazy="{ src: item.goodsImgPath }" alt=""></div>
+              <div class="col c-item-text">
+                <div class="item-text1">{{ item.productName }}</div>
+                <div class="item-text2">{{ item.goodsMsg }}</div>
+                <div class="red item-text3"><span class="middle-price">￥</span>{{ item.goodsPrice }}</div>
+              </div>
+            </a>
+          </div>
+          <div class="tab-end">
+            - END -
+          </div>
         </div>
       </div>
     </div>
@@ -160,7 +162,9 @@ export default {
 
       /* 参数 */
       cateId: '',
-      curPageNo: 0
+      curPageNo: 1,
+
+      busy: true
     };
   },
   computed: {
@@ -184,32 +188,31 @@ export default {
     }
   },
   created() {
-    let self = this
     window.CTJSBridge.LoadAPI("BLAPPSiteQueryAdDeployAPIManager", {
       resourceId: "237300,237301,237302,237304,237306,237307,237310,237311,237320"
     }, {
-      success: function(res) {
+      success: res => {
         // let resData = window.JSON.parse(res).otherResource
         let resData = utils.transData(res).otherResource
-        self.hotBrands = resData[1].advList[0]
-        self.allSlides = resData[2].advList
-        self.hotTwoBrands = resData[3].advList[0]
-        self.asideOne = resData[4].advList[0]
-        self.promotion = resData[5].advList
-        self.asideTwo = resData[6].advList[0]
-        self.brands = resData[7].advList
-        self.eleTabs = resData[8].advList
-        self.cateId = resData[8].advList[0].jumpId
-        self.$nextTick(() => {
-          self.getGoods(null, 0)
-          self.loaded = true
-          self.$loading.close()
+        this.hotBrands = resData[1].advList[0]
+        this.allSlides = resData[2].advList
+        this.hotTwoBrands = resData[3].advList[0]
+        this.asideOne = resData[4].advList[0]
+        this.promotion = resData[5].advList
+        this.asideTwo = resData[6].advList[0]
+        this.brands = resData[7].advList
+        this.eleTabs = resData[8].advList
+        this.cateId = resData[8].advList[0].jumpId
+        this.$nextTick(() => {
+          this.busy = false
+          this.loaded = true
+          this.$loading.close()
         });
       },
       fail: function(data) { console.log(data) },
       progress: function(data) {}
     })
-    self.getGoods('268862', 0)
+    this.getOnceGoods()
   },
   activated() {
     if (this.loaded) {
@@ -225,46 +228,68 @@ export default {
       }
     },
     elTabItem(jumpId) {
-      this.cateId = jumpId
       this.inlineLoading = this.$toast({
         iconClass: 'preloader white',
         message: '加载中',
         duration: 'loading'
       })
-      this.getGoods(null, 0)
+      this.tabRows = []
+      this.cateId = jumpId
+      this.curPageNo = 1
+      this.loadMore()
     },
-    getGoods(cateId, No) {
-      let self = this
-      let thatId = cateId || this.cateId
-      let thatNo = No || this.curPageNo
+    getOnceGoods() {
       let resquestData = {
         "channelSid": "1",
-        "c": "9999" + thatId,
+        "c": "9999268862",
         "searchInfo": {
           "sorCol": "defSort",
           "sorTye": "1",
           "pageModel": {
-            "pageNo": String(thatNo + 1),
+            "pageNo": '1',
             "pageSize": "10"
           }
         }
       }
+      window.CTJSBridge.LoadAPI("BLSearchByKeyWordAPIManager", resquestData, {
+        success: res => {
+          // let resData = window.JSON.parse(res)
+          let resData = utils.transData(res).resultInfo.pageModel.rows
+          if (resData) {
+            this.listRows = resData
+          }
+        },
+        fail: function(data) { console.log(data) },
+        progress: function(data) {}
+      })
+    },
+    loadMore() {
+      this.busy = true
+      let thatId = this.cateId
+      let resquestData = {
+        channelSid: "1",
+        c: "9999" + thatId,
+        searchInfo: {
+          sorCol: "defSort",
+          sorTye: "1",
+          pageModel: {
+            pageNo: String(this.curPageNo ++),
+            pageSize: "10"
+          }
+        }
+      }
+      console.log(resquestData)
       /* 获取商品 */
       window.CTJSBridge.LoadAPI("BLSearchByKeyWordAPIManager", resquestData, {
-        success: function(res) {
-          // let resData = window.JSON.parse(res)
-          let resData = utils.transData(res)
-          if (cateId === '268862') {
-            if (resData.resultInfo.pageModel.rows) {
-              self.listRows = resData.resultInfo.pageModel.rows
-            }
-          } else {
-            if (resData.resultInfo.pageModel.rows) {
-              self.tabRows = resData.resultInfo.pageModel.rows
-            }
+        success: res => {
+          console.log(JSON.parse(res))
+          let resData = window.JSON.parse(res).resultInfo.pageModel.rows
+          if (resData && resData.length) {
+            this.tabRows = this.tabRows.concat(resData)
+            this.busy = false
           }
-          if (self.inlineLoading) {
-            self.inlineLoading.close()
+          if (this.inlineLoading) {
+            this.inlineLoading.close()
           }
         },
         fail: function(data) { console.log(data) },
