fc4e2c9287f0111a1a22699c3da898f71e82af21
AJ <alohajeanne@163.com>
2017-05-22 15:36:04 +0800

    Revert "修改端口"
    This reverts commit deb6f9c8e92e7e6f53d695cf8785ec23bb603422.

--- config/index.js
+++ config/index.js
@@ -26,7 +26,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8088,
+    port: 8080,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
