fa131c3901f29cedf2d9f57f9346ef5e2af7150a
unknown <GJ74@corp.bl.com>
2017-06-01 17:59:10 +0800

    unicom

--- src/api/index.js
+++ src/api/index.js
@@ -184,5 +184,28 @@ export default {
   // 获取售后单列表
   getSaleList: params => {
     return Vue.http.post(baseUrl + '/order/getReturnList.htm', params)
+  },
+  // 联通专区
+  unicom: {
+    // 登陆后查看认证状态
+    authenticate: params => {
+      return Vue.http.post(baseUrl + '/unicom/jn/UnicomVerifyAndGroup.htm', params);
+    },
+    // 点击认证查看认证状态
+    checkAuthentic: params => {
+      return Vue.http.post(baseUrl + '/unicom/jn/UnicomVerifyStatus.htm', params);
+    },
+    // 获取流量列表
+    getTrafficList: params => {
+      return Vue.http.post(baseUrl + '/uniconOrder/queryUniconGroupQuali.htm', params);
+    },
+    // 查看实名认证等级
+    realName: params => {
+      return Vue.http.post(baseUrl + '/memberRNAuthList/queryMemberRNAuthDetail.htm', params);
+    },
+    // 领取流量
+    getTraffic: params => {
+      return Vue.http.post(baseUrl + '/uniconOrder/makeUnicomOrder.htm', params);
+    },
   }
 }
