008551f9409f0f6ca876c4450a760b377615cf5e
薛天玲 <Tianling.Xue@bl.com>
2017-06-09 14:34:05 +0800

    设置支付密码规则

--- src/views/securityCenter/submitPw.vue
+++ src/views/securityCenter/submitPw.vue
@@ -13,6 +13,7 @@
 
 <script>
 import api from './api'
+import utils from 'src/utils'
 import { MD5 } from 'src/md5'
 export default {
 
@@ -20,6 +21,7 @@ export default {
 
   data () {
     return {
+    	// memberToken: '',
     	currentPw: '',
     	member_id: ''
     };
@@ -32,21 +34,83 @@ export default {
   		console.log(this.currentPw)
   		console.log(this.$route.query.pwd)
   		console.log(this.$route.query.member_id)
-  		api.userCenter.revisePayPwd({
-  			memberId: this.$route.query.member_id,
-  			newPwd: MD5(this.currentPw),
-  			pwd: this.$route.query.pwd
-  		}).then(data => {
+  		if (this.currentPw.length != 6) {
   			this.$toast({
-  				message: '设置成功',
-  				position: 'bottom'
+  				message: "请输入6位数字支付密码",
+  				position: "bottom"
   			})
-  			setTimeout(() => {
-  				this.$router.push('/securityCenter')
-  			}, 3000)
-  		})
-  	}
-  }
+  		} else {
+  			let partn = /^\d{6}$/;
+  			if (!partn.exec(this.currentPw)) {
+  			    this.$toast({
+  			    	message: "请输入6位数字支付密码",
+  			    	position: "bottom"
+  			    });
+  			    return;
+  			}
+  			let samePsw = /^(\d)\1+$/;
+  			if (samePsw.exec(this.currentPw)) {
+  			    this.$toast({
+  			    	message: "不允许输入6位相同数字或字符，请重新输入！",
+  			    	position: "bottom"
+  			    });
+  			    return;
+  			}
+  			let incNum = "01234567890";
+  			if (incNum.indexOf(this.currentPw) != -1) {
+  			    this.$toast({
+  			    	message: "不允许输入6位连续数字或字符，请重新输入！",
+  			    	position: "bottom"
+  			    });
+  			    return;
+  			}
+  			let decNum = "09876543210";
+  			if (decNum.indexOf(this.currentPw) != -1) {
+  			    this.$toast({
+  			    	message: "不允许输入6位连续数字或字符，请重新输入！",
+  			    	position: "bottom"
+  			    });
+  			    return;
+  			}
+  			utils.isLogin().then(data => {
+  			  let memberToken = data.member_token
+  			  api.userCenter.getMyInformation({
+  			    member_token: memberToken,
+  			    timestamp: utils.getTimeFormatToday()
+  			  }).then(data => {
+  			    if (data.body.obj) {
+  			    	let phoneNum = JSON.parse(data.body.obj).mobile
+		      		if (phoneNum.indexOf(this.currentPw) != -1) {
+		      		    this.$toast({
+		      		    	message: "不允许与手机号中6位数字相同，请重新输入！",
+		      		    	position: "bottom"
+		      		    });
+		      		    return;
+	  			    } else {
+	  			      api.userCenter.revisePayPwd({
+	  			      	memberId: this.$route.query.member_id,
+	  			      	newPwd: MD5(this.currentPw),
+	  			      	pwd: this.$route.query.pwd
+	  			      }).then(data => {
+	  			      	this.$toast({
+	  			      		message: '设置成功',
+	  			      		position: 'bottom'
+	  			      	})
+	  			      	setTimeout(() => {
+	  			      		this.$router.push('/securityCenter')
+	  			      	}, 3000)
+	  			      })
+	  			    }
+  				} else {
+  					this.$toast(data.body.msg)
+  				}
+  			    }).then(err => {
+  			    console.log(err)
+  			  })
+  			})
+  		}
+	}
+}
 };
 </script>
 
