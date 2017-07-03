9a0945f1c18c0f2d974406918c4a960f829c7091
shenmashi <mashi.shen@bl.com>
2017-07-03 11:05:51 +0800

    udpate

--- src/main.js
+++ src/main.js
@@ -115,14 +115,15 @@ Vue.directive('go-native-goods-similar', {
     let args = {
       pageId: 'similar',
       params: JSON.stringify({
-        goodsSid: binding.value.goodsid,
-        goodsName: binding.value.goodsName,
+        goodsSid: binding.value.goodsId,
+        goodsName: binding.value.goodsMsg,
         salePrice: binding.value.goodsPrice,
-        imgUrlString: binding.value.goodsImageUrl,
+        imgUrlString: binding.value.goodsImgPath,
         isGiftGoods: binding.modifiers.isGiftGoods || false
       })
     }
     el.addEventListener('click', function(e) {
+      console.log(JSON.stringify(args))
       e.stopPropagation();
       if (!el.getAttribute('disabled')) {
         window.CTJSBridge.LoadMethod('BLPageManager', 'NavigateWithStringParams', args)
