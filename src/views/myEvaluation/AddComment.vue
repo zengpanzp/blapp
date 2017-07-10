948759b4a849031ebccc4a9a324384ee0d757f82
陈鹏 <v-peng.chen@bl.com>
2017-07-10 13:41:15 +0800

    init

---
+++ src/views/myEvaluation/AddComment.vue
@@ -0,0 +1,21 @@
+<style lang="scss" src="./css/addComment.scss" scoped></style>
+<template>
+  <h2>addComment</h2>
+</template>
+
+<script>
+export default {
+
+  name: 'AddComment',
+
+  data () {
+    return {
+
+    };
+  },
+  mounted() {
+    this.$loading.close()
+  }
+};
+</script>
+
