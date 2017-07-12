7ac17f0ccb1bd69f0bb6dcdeab942eb1b90ece45
陈鹏 <v-peng.chen@bl.com>
2017-07-12 16:29:15 +0800

    上传图片组件ok

--- src/views/myEvaluation/_comment-upload.vue
+++ src/views/myEvaluation/_comment-upload.vue
@@ -14,6 +14,7 @@
       </div>
       <div class="upload-pic-get lazy-box" v-if="upload.length < maxLength">
         <img :src="require('./i/assess-shoot.png')" alt="" class="lazy">
+        <input type="file" multiple="multiple" accept="image/*" @change="readImage" class="cutfile">
       </div>
     </div>
     <div class="upload-pic-bottom">有图有真相，晒单最多可获20积分!（最多上传<span class="upload-length">{{ maxLength }}</span>张）</div>
@@ -21,23 +22,16 @@
 </template>
 
 <script>
+import api from './api'
+import lrz from 'lrz'
 export default {
 
   name: 'comment-upload',
 
   data () {
     return {
-      upload: [{
-        url: 'http://img.st.iblimg.com/photo-1/1000/190106562_360x360.jpg'
-      }, {
-        url: 'http://img.st.iblimg.com/photo-1/1000/190106562_360x360.jpg'
-      }, {
-        url: 'http://img.st.iblimg.com/photo-1/1000/190106562_360x360.jpg'
-      }, {
-        url: 'http://img.st.iblimg.com/photo-1/1000/190106562_360x360.jpg'
-      }, {
-        url: 'http://img.st.iblimg.com/photo-1/1000/190106562_360x360.jpg'
-      }]
+      upload: [],
+      inlineLoading: null
     };
   },
   props: {
@@ -46,9 +40,65 @@ export default {
       default: 5
     }
   },
-  watch: {
-  },
   methods: {
+    readImage(event) {
+      this.inlineLoading = this.$toast({
+        iconClass: 'preloader white',
+        duration: 'loading',
+        className: 'loading-bg'
+      })
+      if (event.target.files && event.target.files.length > this.maxLength) {
+        this.$modal({
+          title: '提示',
+          content: `最多上传${this.maxLength}张`
+        })
+        return
+      }
+      let files = event.target.files
+      for (let i = 0; i < files.length; i++) {
+        lrz(files[i], {
+          width: 640
+        }).then(res => {
+          console.log(res.formData)
+          // let sourceSize = this.toFixed2(file.size / 1024) // 实际大小
+          let resultSize = this.toFixed2(res.fileLen / 1024) // 压缩后的大小
+          // let scale = parseInt(100 - (resultSize / sourceSize * 100)) // 缩小了多少
+          if (resultSize > 5 * 1024) {
+            this.$modal({
+              title: '提示',
+              content: '图片最大5M'
+            })
+            return
+          }
+          api.upload({
+            appId: 'BL_IBLAPP',
+            base64Content: res.base64.split(",")[1],
+            fileName: new Date().getTime(),
+            mediaType: 'jpg',
+            reSize: 1
+          }).then(res => {
+            if (i == files.length - 1) {
+              this.inlineLoading.close()
+            }
+            console.log('上传图片返回:', JSON.parse(res.body.obj))
+            if (res.body.obj) {
+              let resData = JSON.parse(res.body.obj)
+              this.upload.push({
+                url: resData[0].mediaCephUrl,
+                mediaId: resData[0].mediaId
+              })
+            } else {
+              this.$toast(res.body.msg)
+            }
+          }, err => {
+            this.inlineLoading.close()
+          })
+        })
+      }
+    },
+    toFixed2(num) {
+      return parseFloat(+num.toFixed(2));
+    }
   }
 };
 </script>
@@ -116,5 +166,12 @@ export default {
         color: #e6133c;
       }
     }
+  }
+  .cutfile{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 100;
+    opacity: 0;
   }
 </style>
