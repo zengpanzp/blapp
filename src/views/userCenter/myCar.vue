af8d4c1959ec2e360b106fa3fc3a09185de73fe0
薛天玲 <Tianling.Xue@bl.com>
2017-07-18 15:36:41 +0800

    myCar

--- src/views/userCenter/myCar.vue
+++ src/views/userCenter/myCar.vue
@@ -1,13 +1,20 @@
 <template>
 <div>
 	<div class="car">
-		<img :src="register || require('../..//assets/headPic.png')">
-	</div>
-	<div id="bottom">
-		<div class="bindCar">
-			<bl-button @click="exit" class="btn-exit">绑定车辆</bl-button>
+		<img :src="register || require('./i/no-car.png')">
+		<div class="noCar">
+			暂无绑定车牌
 		</div>
-		
+	</div>
+	<div class="bt">
+		<div class="remind">
+			每个用户最多可以绑定1辆车
+		</div>
+		<div id="bottom">
+			<div class="bindCar">
+				<bl-button @click="exit" class="btn-exit">绑定车辆</bl-button>
+			</div>
+		</div>
 	</div>
 </div>
 </template>
