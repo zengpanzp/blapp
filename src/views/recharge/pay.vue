94f8c1250841d957c96eb50f66e4ea694bcca81e
陈鹏 <v-peng.chen@bl.com>
2017-07-26 10:20:20 +0800

    up

--- src/views/recharge/pay.vue
+++ src/views/recharge/pay.vue
@@ -215,9 +215,9 @@
                       let Pay = require('src/paymodel').default
                       current.inlineLoading.close()
                       Pay.goPay(order, current.getOrderTypeCode(current.typeObj[current.rateType]), (data) => {
-                        let result = JSON.parse(data);
+                        // let result = JSON.parse(data);
                         // 跳转到paysuccess
-                        current.$router.push({path: "/recharge/paysuccess?money=" + result.payAmount + "&orderNo=" + resData.orderNo + "&type=" + current.getOrderTypeCode(current.typeObj[current.rateType])});
+                        current.$router.push({path: "/recharge/paysuccess?money=" + resData.changeMoney + "&orderNo=" + resData.orderNo + "&type=" + current.getOrderTypeCode(current.typeObj[current.rateType])});
                       }, (data) => {
                         // 跳转到详情
                         current.$router.push({path: "/recharge/orderdetail/" + current.getOrderTypeCode(current.typeObj[current.rateType]) + "/" + resData.orderNo});
