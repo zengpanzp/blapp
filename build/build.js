edfab1963d67623be775bb79abf627c1c5b4ba7d
陈鹏 <v-peng.chen@bl.com>
2017-07-31 14:48:34 +0800

    配置文件加入 app universal link配置文件和生成适用web网站的h5.html

--- build/build.js
+++ build/build.js
@@ -19,6 +19,7 @@ shell.rm('-rf', assetsPath)
 shell.mkdir('-p', assetsPath)
 shell.config.silent = true;
 shell.cp('-R', 'static/*', assetsPath)
+shell.cp('-R', 'apple-app-site-association', config.build.assetsRoot)
 shell.config.silent = false
 
 webpack(webpackConfig, function (err, stats) {
